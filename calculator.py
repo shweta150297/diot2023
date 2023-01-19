072886a631689d2dd554a02ac786877d40cf1776
shweta150297 <ssmahale15@gmail.com>
2023-01-19 14:05:10 +0530

    div

--- calculator.py
+++ calculator.py
@@ -6,7 +6,11 @@ def sub(num1,num2):
 
 def mul(num1,num2):
     return num1*num2
+    
+def div(num1,num2):
+    return num1/num2
 
+	
 res_add = add(10,20)
 print(res_add)
 
@@ -15,3 +19,6 @@ print(res_sub)
 
 res_mul = mul(20,10)
 print(res_mul)
+
+res_div = div(200,10)
+print(res_div)
