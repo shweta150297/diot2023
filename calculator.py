a706bd56123d6ffa3601c79dde9c284dcfb254d3
shweta150297 <ssmahale15@gmail.com>
2023-01-19 14:27:46 +0530

    change

--- calculator.py
+++ calculator.py
@@ -8,8 +8,10 @@ def mul(num1,num2):
     return num1*num2
     
 def div(num1,num2):
-    return num1/num2
-
+	try:
+  	    return num1/num2
+	except ZeroDivisionError:
+	     print("Division by zero not allowed")
 	
 res_add = add(10,20)
 print(res_add)
